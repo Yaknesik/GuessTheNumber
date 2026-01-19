a44598617194c93e255275386b603be7dfc1c2e5
Yaknesik <81623470+Yaknesik@users.noreply.github.com>
2026-01-19 09:50:08 +0100

    Dodano main.cpp

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "============================================\nWitamy w grze liczbowej \"Guess The Number!\"\n============================================";
+};
