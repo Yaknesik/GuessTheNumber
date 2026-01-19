be30a88f95cb60b2d0398367ed2b9b9449d51fdc
Yaknesik <81623470+Yaknesik@users.noreply.github.com>
2026-01-19 11:47:47 +0100

    game.cpp i README.md - Dodanie źródeł do README.md, na których się opierałem. - Utworzenie w game.cpp logiki gry (oraz trybu extreme)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GuessTheNumber
 Gra w C++ "Zgadnij liczbę!"
+
+
+
+Źródła, na których się opierałem:
+https://www.w3schools.com/cpp/
+https://www.codecademy.com/
--- game.cpp
+++ game.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 using namespace std;
 
 
 void startGame(int difficulty) {
     srand(time(0));
+
+
+    vector<string> tooLow {
+        "Za mało!",
+        "Spróbuj większą liczbę!",
+        "Nie, to za niska liczba!",
+        "Więcej!",
+        "Podnieś wartość!",
+        "Nie trafione, jest za mało!",
+        "Jeszcze trochę większa!",
+        "Za mało, próbuj dalej!",
+        "Nie, liczba jest większa!",
+        "Nie zgadłeś, spróbuj większą!"
+    };
+
+    vector<string> tooHigh {
+        "Za dużo!",
+        "Spróbuj mniejszą liczbę!",
+        "Nie, to zbyt wysoka liczba!",
+        "Mniej!",
+        "Obniż wartość!",
+        "Nie trafione, jest za dużo!",
+        "Jeszcze trochę mniejsza!",
+        "Za dużo, próbuj dalej!",
+        "Nie, liczba jest niższa!",
+        "Nie zgadłeś, spróbuj mniejszą!"
+    }
+
 
     int maxNumber;
     bool extremeMode = false;
@@ -19,4 +48,46 @@ void startGame(int difficulty) {
             return;
     }
 
-}
+    int randomNumber =  rand() % maxNumber + 1;
+
+    int guess;
+
+    int tries = 0;
+
+    cout << "\n\n\n\n\n\n\n\n\nZgadnij liczbę w zakresie od 1 do " + maxNumber << "!\n";
+
+    do {
+        cout << "Próba nr " << tries << " Twój strzał:";
+        cin >> guess
+
+        if(!cin || guess <= 0 || guess > maxNumber) {
+            cout << "BŁĄD! Proszę wpisać tylko liczbę (nie używamy liter!), która jest w zakresie od 1 do" + maxNumber << "!\n";
+            cin.clear();
+            cin.ignore(1000, '\n');
+            if(extreme) {
+                tries++;
+                cout << "W trybie ekstremalnym, nawet pomyłki liczą się jako próby ;)!\n";
+            };
+            continue;
+        }
+
+        tries++;
+
+        if(guess < number) {
+            int randomLow = rand() % tooLow.size();
+            cout << tooLow[randomLow] << endl;
+        };
+
+        else if (guess > number) {
+            int randomHigh = rand() % tooHigh.size();
+            cout << tooHigh[randomHigh] << endl;
+        };
+        else 
+            cout << "Brawo! Odgadnięto liczbę \"" << number << "\" w " << tries << "próbach!\n";
+
+            if(extreme && tries % 5 == 0 %% guess != number) {
+                number = rand() % maxNumber + 1;
+                cout << "LICZBA ZOSTAŁA ZMIENIONA!!!\n";
+            }
+    } while (guess != number);
+};
